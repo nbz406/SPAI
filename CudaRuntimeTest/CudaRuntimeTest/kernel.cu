954eb28987ec9dd7a50848a448fb1a2e9499e550
LAPTOP-4GDO7VV2\emilv <emilvr@live.dk>
2023-04-17 08:54:37 +0200

    stuffe

--- CudaRuntimeTest/CudaRuntimeTest/kernel.cu
+++ CudaRuntimeTest/CudaRuntimeTest/kernel.cu
@@ -95,53 +95,182 @@ int main()
 
     Utils::create_identity_csc(n_rows, csc_col_ptr_M, csc_val_M, csc_row_ind_M);
 
+    int Js_size = 0;
+    int Is_size = 0;
+    int A_hats_size = 0;
+    int maxn1 = 0;
+    int maxn2 = 0;
+    int minn2 = n_cols;
+    int minn1 = n_cols;
+
+    int* n2s = static_cast<int*>(malloc(sizeof(int) * n_cols));
+    int* n1s = static_cast<int*>(malloc(sizeof(int) * n_cols));
+    int* Js_inds = static_cast<int*>(malloc(sizeof(int) * n_cols));
+    int* Is_inds = static_cast<int*>(malloc(sizeof(int) * n_cols));
+    int* A_hats_inds = static_cast<int*>(malloc(sizeof(int) * n_cols));
+
     for (int k = 0; k < n_cols; k++)
     {
-	    const int beg = csc_col_ptr_M[k]; // points to start of sub-array with column k
-	    const int end = csc_col_ptr_M[k + 1]; // points to end of sub-array with column k
+        const int beg = csc_col_ptr_M[k]; // points to start of sub-array with column k
+        const int end = csc_col_ptr_M[k + 1]; // points to end of sub-array with column k
 
-        // Construct J by getting all row indices from beg to end 
-        int n2 = end - beg;
-        int* J = static_cast<int*>(malloc(sizeof(int) * n2));
-        for (int j = 0; j < n2; j++)
+        // Construct J by getting all row indices from beg to end
+        const int n2 = end - beg;
+        n2s[k] = n2;
+        if (n2 > maxn2)
         {
-            J[j] = csc_row_ind_M[j + beg];
+            maxn2 = n2;
+        }
+        if (n2 < minn2)
+        {
+            minn2 = n2;
+        }
+    }
+    // scan n2s
+    for (int k = 0; k < n_cols; k++)
+    {
+        Js_inds[k] = Js_size;
+        Js_size += n2s[k];
+    }
+    float avgn2 = static_cast<float>(Js_size) / static_cast<float>(n_cols);
+    int* Js = static_cast<int*>(malloc(sizeof(int) * Js_size));
+
+    for (int k = 0; k < n_cols; k++)
+    {
+        const int beg = csc_col_ptr_M[k]; // points to start of sub-array with column k
+        for (int j = 0; j < n2s[k]; j++)
+        {
+            Js[Js_inds[k]] = csc_row_ind_M[j + beg];
         }
 
         // Construct I: nonzero rows of A[:,J]
         // We have to allocate n2 = reduce (+) (map (\j -> shape_A[j]) J) = reduce (+) (map (\j -> csc_col_ptr_A[j+1]-csc_col_ptr_A[j]) J)
         int n1 = 0;
-    	for (int j = 0; j < n2; j++)
+        for (int j = 0; j < n2s[k]; j++)
         {
-            const int col_ind = J[j];
+            const int col_ind = Js[Js_inds[k]];
             n1 += csc_col_ptr_A[col_ind + 1] - csc_col_ptr_A[col_ind];
         }
-
+        n1s[k] = n1;
+        if (n1 > maxn1)
+        {
+            maxn1 = n1;
+        }
+        if (n1 < minn1)
+        {
+            minn1 = n1;
+        }
+    }
+    // Scan n1s
+    int Qs_size = 0;
+    /*
+    int* Qs_inds = static_cast<int*>(malloc(sizeof(int) * n_cols));
+    */
+    for (int k = 0; k < n_cols; k++)
+    {
+        const int n1 = n1s[k];
+        Is_inds[k] = Is_size;
+        Is_size += n1;
+        A_hats_inds[k] = A_hats_size;
+        A_hats_size += n1 * n2s[k];
+        /*
+        Qs_inds[k] = Qs_size;
+        Qs_size += n1 * n1;
+        */
+    }
+#if _DEBUG
+    float avgn1 = static_cast<float>(Is_size) / static_cast<float>(n_cols);
+#endif
+    int* Is = static_cast<int*>(malloc(sizeof(int) * Is_size));
+    double* A_hats = static_cast<double*>(malloc(sizeof(double) * A_hats_size));
+    for (int k = 0; k < n_cols; k++)
+    {
         // Maybe find max dimensions between columns to make QR batched work
-
         // Get indices from csc_row_ind_A starting from the column pointers from csc_col_ptr_A[J]
         // Construct dense A[I,J] in column major format to be used in batched QR decomposition.
-        int* I = static_cast<int*>(malloc(sizeof(int) * n1));
-        double* A_hat = static_cast<double*>(malloc(sizeof(double) * n1 * n2));
-        int i_ind = 0;
-        for (int j = 0; j < n2; j++)
+        for (int j = 0; j < n2s[k]; j++)
         {
-            const int col_ind = J[j];
+            const int col_ind = Js[Js_inds[k]];
             const int col_start = csc_col_ptr_A[col_ind];
             const int col_end = csc_col_ptr_A[col_ind + 1];
             for (int i = col_start; i < col_end; i++)
             {
-                I[i_ind] = csc_row_ind_A[i];
-                A_hat[IDX2C(i_ind, j, n1)] = csc_val_A[i];
-                i_ind++;
+                Is[Is_inds[i - col_start]] = csc_row_ind_A[i];
+                A_hats[A_hats_inds[k] + IDX2C(i - col_start, j, n1s[k])] = csc_val_A[i];
             }
         }
 
-        const int batchsize = 1;
-        const int ltau = std::max(1, std::min(n1, n2));
+    }
+    /*
+    double* Qs = static_cast<double*>(malloc(sizeof(double) * Qs_size));
 
-        double* dA_hat;     cudacall(cudaMalloc((void**)&dA_hat, batchsize * n1 * n2 * sizeof(*dA_hat)));
-        cudacall(cudaMemcpy(dA_hat, A_hat, n1 * n2 * sizeof(dA_hat), cudaMemcpyHostToDevice));
+    for (int k = 0; k < n_cols; k++)
+    {
+        const int n1 = n1s[k];
+        for (int j = 0; j < n1; j++)
+        {
+			for (int i = 0; i < n1; i++)
+			{
+				if (i == j)
+				{
+                    Qs[Qs_inds[k] + IDX2C(i, j, n1)] = 1.0;
+				}
+				else
+				{
+                    Qs[Qs_inds[k] + IDX2C(i, j, n1)] = 0.0;
+				}
+			}
+        }
+    }
+
+    // attempt at QR
+    for (int ok = 0; ok < n_cols; ok++)
+    {
+        const int m = n1s[ok];
+        const int n = n2s[ok];
+        double* A_hat = &A_hats[A_hats_inds[ok]];
+        for (int k = 0; k < n; k++)
+        {
+	        //col ak from k to m. house n : h_n = m - k
+            const int col_n = m - k;
+            double *x =  &A_hat[IDX2C(0, k, m)];
+
+            //dot
+            double dot = 0.0;
+            for (int i = 0; i < col_n; i++)
+            {
+                dot += x[i] * x[i];
+            }
+            double* v = static_cast<double*>(malloc(sizeof(double) * col_n));
+            v[0] = x[0] - sqrt(dot);
+            for (int i = 1; i < col_n; i++)
+            {
+                v[i] = x[i];
+            }
+            double dotprime = dot - pow(x[0], 2) + pow(v[0], 2);
+            double beta = !iszero(dotprime) ? 2.0 / dotprime : 0.0;
+
+            double* vvt = static_cast<double*>(malloc(sizeof(double) * col_n * col_n));
+            double* Bvvt = static_cast<double*>(malloc(sizeof(double) * col_n * col_n));
+            for (int j = 0; j < col_n; j++)
+            {
+                for (int i = 0; i < col_n; i++)
+                {
+                    vvt[IDX2C(i, j, col_n)] = v[i] * v[j];
+                    Bvvt[IDX2C(i, j, col_n)] = vvt[IDX2C(i, j, col_n)] *beta;
+                }
+            }
+            //matmul
+        }
+    }
+    */
+    for (int k = 0; k < n_cols; k++)
+    {
+        const int batchsize = 1;
+        const int ltau = std::max(1, std::min(n1s[k], n2s[k]));
+
+        double* dA_hat;     cudacall(cudaMalloc((void**)&dA_hat, batchsize * n1s[k] * n2s[k] * sizeof(*dA_hat)));
+        cudacall(cudaMemcpy(dA_hat, &A_hats[A_hats_inds[k]], n1s[k] * n2s[k] * sizeof(dA_hat), cudaMemcpyHostToDevice));
 
         double* d_TAU;      cudacall(cudaMalloc((void**)&d_TAU, batchsize * ltau * sizeof(double)));
 
@@ -149,7 +278,7 @@ int main()
 
         for (int i = 0; i < batchsize; i++)
         {
-            h_A_hatArray[i] = dA_hat + i * n1 * n2;
+            h_A_hatArray[i] = dA_hat + i * n1s[k] * n2s[k];
             h_TauArray[i] = d_TAU + i * ltau;
         }
 
@@ -160,21 +289,24 @@ int main()
         cudaMemcpy(d_Aarray, h_A_hatArray, sizeof(h_A_hatArray), cudaMemcpyHostToDevice);
         cudaMemcpy(d_TauArray, h_TauArray, sizeof(h_TauArray), cudaMemcpyHostToDevice);
         int info;
-        cublascall(cublasDgeqrfBatched(handle, n1, n2, d_Aarray, n1, d_TauArray, &info, batchsize));
+        cublascall(cublasDgeqrfBatched(handle, n1s[k], n2s[k], d_Aarray, n1s[k], d_TauArray, &info, batchsize));
 
-        cudacall(cudaMemcpy(A_hat, dA_hat, batchsize * n1 * n2 * sizeof(double), cudaMemcpyDeviceToHost));
+        double* tau = static_cast<double*>(malloc(sizeof(double) * ltau));
+        cudacall(cudaMemcpy(&A_hats[A_hats_inds[k]], dA_hat, batchsize * n1s[k] * n2s[k] * sizeof(double), cudaMemcpyDeviceToHost));
+        cudacall(cudaMemcpy(tau, d_TAU, batchsize * ltau * sizeof(double), cudaMemcpyDeviceToHost));
+
+
 
         // Free all allocations
-        free(A_hat);
-        free(I);
-        free(J);
         cudacall(cudaFree(dA_hat));
         cudacall(cudaFree(d_TAU));
         cudacall(cudaFree(d_Aarray));
         cudacall(cudaFree(d_TauArray));
     }
 
-
+    free(A_hats_inds); free(A_hats);
+    free(n1s); free(Js); free(Js_inds);
+    free(n2s); free(Is); free(Is_inds);
     free(csc_col_ptr_A); free(csc_val_A); free(csc_row_ind_A);
     free(csc_col_ptr_M); free(csc_val_M); free(csc_row_ind_M);
     cublasDestroy_v2(handle);
--- SpaiTest.py
+++ SpaiTest.py
@@ -8,19 +8,84 @@ from scipy.sparse import csr_matrix
 import random
 import heapq
 import functools
+import math
 
 from io import StringIO
 from scipy.io import mmread
 
-N = 1000
-n_most_profitable_indices = 5
-epsilon = 0.5
-maxiter = 10
+from typing import Union
+
+def householder(A):
+    (n,m)=A.shape
+    p=min(n,m)
+    alpha=np.zeros(m)
+    for j in range(0,p):
+        alpha[j]=np.linalg.norm(A[j:,j])*np.sign(A[j,j])
+        if (alpha[j]!=0):
+            beta=1/math.sqrt(2*alpha[j]*(alpha[j]+A[j,j]))
+            A[j,j]=beta*(A[j,j]+alpha[j])
+            A[j+1:,j]=beta*A[j+1:,j]
+            for k in range(j+1,m):
+                gamma=2*A[j:,j].dot(A[j:,k])
+                A[j:,k]=A[j:,k]-gamma*A[j:,j]
+    return A,alpha
+
+def loese_householder(H,alpha,b):
+    (n,m)=H.shape
+    b=b.copy()
+    x=np.zeros(n)
+    # b=Q^t b.
+    for j in range(0,n):
+        b[j:]=b[j:]-2*(H[j:,j].dot(b[j:]))*H[j:,j]
+    # Auflösen von Rx=b.
+    for i in range(0,n):
+        j=n-1-i
+        b[j]=b[j]-H[j,j+1:].dot(x[j+1:])
+        x[j]=-b[j]/alpha[j]
+    return x
+
+def householder_vectorized(a):
+    """Use this version of householder to reproduce the output of np.linalg.qr 
+    exactly (specifically, to match the sign convention it uses)
+    
+    based on https://rosettacode.org/wiki/QR_decomposition#Python
+    """
+    v = a / (a[0] + np.copysign(np.linalg.norm(a), a[0]))
+    v[0] = 1
+    tau = (2 / (v.T @ v))[0,0]
+    
+    return v,tau
+
+def qr_decomposition(A: np.ndarray) -> Union[np.ndarray, np.ndarray]:
+    m,n = A.shape
+    R = A.copy()
+    Q = np.identity(m)
+    
+    for j in range(0, n):
+        # Apply Householder transformation.
+        v, tau = householder_vectorized(R[j:, j, np.newaxis])
+        
+        H = np.identity(m)
+        H[j:, j:] -= tau * np.outer(v, v)
+        R = H @ R
+        Q = H @ Q
+        
+    return Q.T, R
+
+n_most_profitable_indices = 5 # bounded from 3 to 8
+epsilon = 0.2 # 0.1 to 0.5 according to https://mediatum.ub.tum.de/doc/1107998/426923.pdf#page=64&zoom=100,117,534
+maxiter = 5 # 1 to 5
 
 # M is just a sparse identity matrix
-M = scipy.sparse.identity(N, format='csr')
 # A is a csr format of the matrix that we wish to invert
-A = scipy.sparse.csr_matrix(mmread("CudaRuntimeTest\sherman1.mtx"))
+A = scipy.sparse.csr_matrix(mmread("CudaRuntimeTest\orsirr_2.mtx"))
+N = A.shape[0]
+M = scipy.sparse.identity(N, format='csr')
+#M[1,0] = 1
+#M[2,0] = 1
+#M[3,0] = 1
+#M[4,0] = 1
+#M[5,0] = 1
 AInv = scipy.sparse.linalg.inv(A)
 
 def permutation(set, n, settilde, ntilde, mode="col"):
@@ -49,8 +114,6 @@ for k in range(0, M.shape[1]):
     J = m_k.nonzero()[0] # gets row inds of nonzero
     n2 = J.size
 
-    print(J)
-
     # Calculate A(.,J)
     A_J = A[:,J]
 
@@ -64,18 +127,36 @@ for k in range(0, M.shape[1]):
     # Compute ehat_k
     ehat_k = e_k[I]
 
-    A_IJ = np.hstack((np.vstack((A_IJ, np.zeros((n1,n2)))),np.vstack((np.zeros((n1,n2)), np.zeros((n1,n2))))))
-
-    print(A_IJ)
+    #A_IJ = np.hstack((np.vstack((A_IJ, np.zeros((n1,n2)))),np.vstack((np.zeros((n1,n2)), np.zeros((n1,n2))))))
 
     # Compute QR decomp. R_1 upper triangular n1 x n1 matrix. 0 is an (n1 − n2)×n2 zero matrix. Q1 is m×n, Q2 is m×(m − n)
-    Q, R = np.linalg.qr(A_IJ, mode="complete")
+    (h, tau) = np.linalg.qr(A_IJ, mode="raw")
+    Q_, R_ = np.linalg.qr(A_IJ, mode="complete")
+    #print("R: ",R)
+
+#Foreach column in A_IJ
+    #R = A_IJ
+    #Q = np.identity(n1)
+    #vs = h
+    #print(A_IJ.shape)
+    #for i in range(0, n2):
+    #    v = h[i].T
+    #    v = v[i:]
+    #    v[0] = 1
+    #    print(v)
+    #    vvT = np.outer(v,v)
+    #    Q[0:n1, i:n1] = Q[0:n1, i:n1] - tau[i] * np.matmul(Q[0:n1, i:n1] , vvT)
+    #    R[i:n1, i:n2] = R[i:n1,i:n2] - tau[i] * np.matmul(vvT, R[i:n1,i:n2])
+    
+    Q, R = qr_decomposition(A_IJ)
+
+    H,alpha=householder(A_IJ.copy())
+    
+    print(H)
+    print(R)
+
     R_1 = R[:n2,:n2]
-
     Q = Q[:n1,:n1]
-
-    print("Q: ",Q)
-    print("R: ",R_1)
 
     # Compute mhat_k
     chat_k = Q.T * ehat_k
@@ -91,9 +172,9 @@ for k in range(0, M.shape[1]):
 
     while r_norm > epsilon and iter < maxiter:
         iter += 1
-        print(iter)
+        print("iter: ",iter)
         # Calculate L
-        L = np.nonzero(r)[0]
+        L = np.union1d(I,k) #np.nonzero(r)[0] # Lk ← Ik ∪ {k} ? from paper https://mediatum.ub.tum.de/doc/1107998/426923.pdf#page=64&zoom=100,117,534
 
         # Calculate Jtilde: All of the the new column indices of A that appear in all
         # L rows but not in J
@@ -105,11 +186,10 @@ for k in range(0, M.shape[1]):
             Jtilde = np.union1d(Jtilde,N_l)
 
         # Calculate the new norm of the modified residual and record the indices j
+        # Could get A_J and each column is a j
         j_rho_pairs = []
         for j in Jtilde:
-            e_j = np.matrix([0]*N).T
-            e_j[j] = 1
-            Ae_j = A * e_j
+            Ae_j = A[:,j].todense()
             Ae_jnorm = np.linalg.norm(Ae_j)
             rTAe_j = r.T * Ae_j
             rho_jsquared = r_norm*r_norm - (rTAe_j * rTAe_j) / (Ae_jnorm * Ae_jnorm)
@@ -150,8 +230,10 @@ for k in range(0, M.shape[1]):
         B_1 = Au[:n2,:]
         B_2 = np.vstack((Au[n2:n1,:], AItildeJtilde.todense()))
 
+        print(A[np.ix_(np.union1d(I, Itilde), np.union1d(J, Jtilde))].shape)
         # QR decompose B2
-        Q_B, R_B = np.linalg.qr(B_2, mode="complete") 
+        QB, R_B = qr_decomposition(B_2)
+        QB, R_B = np.linalg.qr(B_2, mode="complete")
 
         # Matrices to use in new QR
         In1tilde = np.identity(n1tilde)
@@ -161,10 +243,10 @@ for k in range(0, M.shape[1]):
 
         # for the new Q, which is a matrix product
         firstmat17 = np.hstack((np.vstack((Q, n1tilden1zeros)), np.vstack((n1n1tildezeros, In1tilde))))
-        secondmat17 = np.hstack((np.vstack((In2, np.zeros((n1-n2+n1tilde,n2)))), np.vstack((np.zeros((n2,n1-n2+n1tilde)), Q_B))))
+        secondmat17 = np.hstack((np.vstack((In2, np.zeros((n1-n2+n1tilde,n2)))), np.vstack((np.zeros((n2,n1-n2+n1tilde)), QB))))
 
         # Construct R and Q by stacking and matrix products
-        R = np.hstack((np.vstack((R_1, np.zeros((n1tilde + n1 - n2, n2)))), np.vstack((B_1, R_B))))
+        R_1 = np.hstack((np.vstack((R_1, np.zeros((n2tilde, n2)))), np.vstack((B_1, R_B[0:n2tilde])))) # don't need entire R
         Q = firstmat17 * secondmat17
 
         # New J and I
@@ -172,13 +254,16 @@ for k in range(0, M.shape[1]):
         n2 = J.size
         I = np.union1d(Itilde, I) # Itilde
         n1 = I.size
-        R_1 = R[0:n2,:]
-            
+        #R_1 = R[0:n2,:]
+    
         etilde_k = Pr * e_k[I]
-        
-        chat_k = Q.T * etilde_k # selects the 
-        mtilde_k = inv(R_1) * chat_k[0:n2,:]
+        rowofQ = etilde_k.nonzero()[0]
 
+        print("-------", rowofQ)
+        chat_k = Q.T * etilde_k # selects the kth column but permuted.
+        mtilde_k = inv(R_1) * chat_k[0:n2,:] #inv(R_1) * chat_k[0:n2,:]
+
+        
         m_k[J] = Pc * mtilde_k
 
         # Permute Q and R to be used in next iteration
@@ -188,6 +273,8 @@ for k in range(0, M.shape[1]):
         # Compute residual r
         r = A[:,J] * m_k[J] - e_k
         r_norm = np.linalg.norm(r)
+
+        print(r_norm)
 
     # Place result column in matrix
     M[:,k] = m_k
